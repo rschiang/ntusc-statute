748f043eb1f7582415aec4515f2fd6420c3dd9f2
Poren Chiang <ren.chiang@gmail.com>
2017-03-07 12:44:55 +0800

    Initial parser definition

--- scripts/statute.py
+++ scripts/statute.py
@@ -1,4 +1,5 @@
 # statue.py - parses and stores statute text
+import re
 from io import StringIO
 
 class Act(object):
@@ -46,6 +47,38 @@ def parse_act(buf):
                 assert line
                 act.history.append(line.strip())
 
+        while True:
+            indented_line = buf.readline().rstrip()
+            if not indented_line:
+                break
+
+            line = indented_line.lstrip()
+            indent = len(indented_line) - len(line)
+
+            if indent == 0:    # Chapter title
+                m = re.match(r'^(\S+)　(\S+)', line)
+                assert m
+                chapter = Chapter(number=m.group(1), caption=m.group(2))
+                act.articles.append(chapter)
+            elif indent == 2:  # Article
+                m = re.match(r'^(\S+)　【(\S+)】', line)
+                assert m
+                article = Article(number=m.group(1), caption=m.group(2))
+                act.articles.append(article)
+            elif indent == 4:  # Paragraph
+                paragraph = Paragraph(caption=line)
+                article.subitems.append(paragraph)
+            elif indent == 6:  # Subsection
+                subsection = Subsection(caption=line)
+                paragraph.subitems.append(subsection)
+            elif indent == 8:  # Item
+                subsection.subitems.append(line)
+
         return act
     except AssertionError:
-        pass  # parse failed, malformed file
+        raise  # parse failed, malformed file
+
+
+if __name__ == '__main__':
+    f = open('../example.txt', 'r')
+    print(parse_act(f))
