87b7daf19e150c5f1584e804d1cc31efb6c818f4
Poren Chiang <ren.chiang@gmail.com>
2017-03-10 22:53:34 +0800

    Try integrate converter to generator, failed

--- scripts/generator.py
+++ scripts/generator.py
@@ -2,6 +2,7 @@
 # generator.py - iterate and generate statute
 import glob
 import os.path
+from converter import convert_interpretation
 from parser import parse_act
 from renderer import HtmlRenderer
 from statute import Category
@@ -11,12 +12,18 @@ class CategoryTask(Category):
         self.folders = folders or []
         super().__init__(slug=slug, caption=caption, label=label, acts=acts)
 
+class ConverterTask(object):
+    def __init__(self, bookmark_id, path):
+        self.bookmark_id = bookmark_id
+        self.path = path
+
 
 CATEGORIES = [
     CategoryTask(slug='supreme', caption='基本法與綜合性法規', label='基本法', folders=['1_基本法', '2_綜合性法規']),
     CategoryTask(slug='admin', caption='行政部門', label='行政', folders=['3_會長暨行政部門篇']),
     CategoryTask(slug='legis', caption='立法部門', label='立法', folders=['4_立法部門篇']),
     CategoryTask(slug='judicial', caption='司法部門', label='司法', folders=['5_司法部門篇']),
+    CategoryTask(slug='interpret', caption='學生法院解釋文', label='解釋', folders=['6_學生法院解釋']),
     CategoryTask(slug='appendix', caption='附錄', label='司法', folders=['7_附錄', '../../statute/appendix/']),
     ]
 
@@ -31,15 +38,21 @@ def generate(path='source/laws/', output='statute.html'):
     # Read in
     for category in CATEGORIES:
         count = 0
+        use_converter = (category.slug == 'interpret')
         for pattern in (os.path.join(path, f, '*.txt') for f in category.folders):
             for file_path in sorted(glob.glob(pattern)):
                 count += 1
-                print(file_path)
-                with open(file_path, 'r') as file_buf:
-                    act = parse_act(file_buf)
-                    act.bookmark_id = '{}_{:02}'.format(category.slug, count)
-                    act.update_bookmark_id()
-                    category.acts.append(act)
+                bookmark_id = '{}_{:02}'.format(category.slug, count)
+                if use_converter:
+                    task = ConverterTask(bookmark_id, file_path)
+                    category.acts.append(task)
+                else:
+                    print(file_path)
+                    with open(file_path, 'r') as file_buf:
+                            act = parse_act(file_buf)
+                            act.bookmark_id = bookmark_id
+                            act.update_bookmark_id()
+                            category.acts.append(act)
 
     # Build cover and TOC
     renderer.render_section('intro', '我們貢獻這所大學于宇宙之精神。')
@@ -51,8 +64,14 @@ def generate(path='source/laws/', output='statute.html'):
     # Render individual categories
     for category in CATEGORIES:
         renderer.render_category(category)
-        for act in category.acts:
-            renderer.render_act(act)
+        if category.slug == 'interpret':
+            for task in category.acts:
+                print(task.path)
+                with open(task.path, 'r') as file_buf:
+                    convert_interpretation(source=file_buf, dest=buf, bookmark_id=task.bookmark_id)
+        else:
+            for act in category.acts:
+                renderer.render_act(act)
 
     renderer.render_tail()
     buf.close()
