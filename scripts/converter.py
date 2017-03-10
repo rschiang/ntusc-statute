33f1fdc3b8d6024c710aea8e6439f1304de24ca6
Poren Chiang <ren.chiang@gmail.com>
2017-03-10 22:38:05 +0800

    Implement interpretation converter

--- scripts/converter.py
+++ scripts/converter.py
@@ -4,10 +4,14 @@ import sys
 from io import StringIO
 from utils import normalize_spaces
 
+RE_CJK_NUMERICS = r'〇ㄧ一二三四五六七八九十'
 RE_REPUBLIC_DATE_FORMAT = re.compile(r'(中華)?民國\s*(?P<year>\d+)\s*年\s*(?P<month>\d+)\s*月\s*(?P<day>\d+)\s*日')
 RE_META_REMARK_FORMAT = re.compile(r'(（(首席|註[^）]+)）)')
-RE_NOTABLE_REMARK_FORMAT = re.compile(r'([（\(](以?下簡?稱|[備附]註|註\s*[\d〇ㄧ一二三四五六七八九十]+)[^）]+[）\)])')
+RE_NOTABLE_REMARK_FORMAT = re.compile(r'([（\(](以?下簡?稱|[備附]註|註\s*[\d' + RE_CJK_NUMERICS + r']+)[^）]*[）\)])')
 RE_CITATION_FORMAT = re.compile(r'([（\(][^）\)]+參照[）\)])')
+RE_HEADING_FORMAT = re.compile(r'([（\(][' + RE_CJK_NUMERICS + r'][）\)]、?|[' + RE_CJK_NUMERICS + r']、|\d\. )[^。]+$')
+RE_SUBHEADING_FORMAT = re.compile(r'^([（\(][' + RE_CJK_NUMERICS + r'A-Z][）\)]、?|[' + RE_CJK_NUMERICS + r']、|\d\. )[^。；]+：')
+RE_FOOTER_FORMAT = re.compile(r'^(註\s[\d' + RE_CJK_NUMERICS + r']+：.+)$')
 RE_CHAPTER_FORMAT = re.compile(r'(解釋(文|理由)|聲請|公告)書?')
 
 def apply_meta_remarks(text):
@@ -18,6 +22,38 @@ def apply_notable_remarks(text):
 
 def apply_citation(text):
     return RE_CITATION_FORMAT.sub('<cite>\1</cite>', text)
+
+def render_chapter(dest, text):
+    dest.write('<h5>')
+    dest.write(normalize_spaces(text))
+    dest.write('</h5>\n')
+
+def render_heading(dest, text):
+    dest.write('<h6>')
+    dest.write(normalize_spaces(text))
+    dest.write('</h6>\n')
+
+def render_paragraph(dest, text):
+    # Heading sniffing
+    if RE_HEADING_FORMAT.fullmatch(text):
+        render_heading(dest, text)
+        return
+
+    # Apply formats
+    text = normalize_spaces(text)
+    text = apply_citation(text)
+    text = apply_notable_remarks(text)
+
+    # Sniff subheading
+    text = RE_SUBHEADING_FORMAT.sub(r'<span class="subheading">\1</span>', text)
+
+    # Footnote sniffing
+    if RE_FOOTER_FORMAT.fullmatch(text):
+        dest.write('<p class="footnote">')
+    else:
+        dest.write('<p>')
+    dest.write(text)
+    dest.write('</p>\n')
 
 def convert_interpretation(source, dest, bookmark_id=None):
     if isinstance(source, str):
@@ -68,15 +104,18 @@ def convert_interpretation(source, dest, bookmark_id=None):
             indent = len(indented_line) - len(line)
 
             if indent == 0:
-                heading_match = RE_CHAPTER_FORMAT.fullmatch(line)
-                if heading_match:
-                    pass
+                chapter_match = RE_CHAPTER_FORMAT.fullmatch(line)
+                if chapter_match:
+                    render_chapter(dest, line)
                 else:
-                    pass
+                    render_heading(dest, line)
             if indent == 2:
-                pass
+                if not line.startswith('邱丞正、'):  # workaround for bad indent
+                    render_heading(dest, line)
+                else:
+                    render_paragraph(dest, line)
             else:
-                pass
+                render_paragraph(dest, line)
 
         dest.write('</article>\n')
 
@@ -86,5 +125,10 @@ def convert_interpretation(source, dest, bookmark_id=None):
 
 
 if __name__ == '__main__':
+    from renderer import HtmlRenderer
+    renderer = HtmlRenderer(buf=sys.stdout)
+
     f = open('source/laws/6_學生法院解釋/17_第10號.txt', 'r')
+    renderer.render_head()
     convert_interpretation(source=f, dest=sys.stdout)
+    renderer.render_tail()
