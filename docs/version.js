e1d1fa6d06187bdb3a93a29080fef19b15aa426a
Poren Chiang <ren.chiang@gmail.com>
2018-02-27 04:31:50 +0800

    Use old good XHR, fix styles

--- docs/version.js
+++ docs/version.js
@@ -1,12 +1,15 @@
-fetch('https://rschiang.github.io/ntusc-statute/version.json').then(function(response) {
-  var version = JSON.parse(response.body);
+var xhr = new XMLHttpRequest();
+xhr.addEventListener('load', function() {
+  var version = JSON.parse(this.responseText);
   var footer = document.querySelector('footer');
   var root = document.createElement('div');
-  root.setAttribute('class', 'version-info');
+  root.setAttribute('id', 'version-info');
   root.innerHTML = '法規版本 '+
-  '<a class="commit" href="https://github.com/ntu-student-congress/tortue/commit/'+version.statute+'">'+version.statute.sub(0,7)+'</a>'+
+  '<a class="commit" href="https://github.com/ntu-student-congress/tortue/commit/'+version.statute+'">'+version.statute.substring(0,7)+'</a>'+
   '，建置版本 '+
-  '<a class="commit" href="https://github.com/rschiang/ntusc-statute/commit/'+version.build+'">'+version.build.sub(0,7)+'</a>'+
-  '('+ version.date +')';
+  '<a class="commit" href="https://github.com/rschiang/ntusc-statute/commit/'+version.build+'">'+version.build.substring(0,7)+'</a>'+
+  ' ('+ version.date +')';
   footer.appendChild(root);
-})
+});
+xhr.open('GET', (/\/(index\.html)?$/.test(location.href) ? '' : '../') + 'version.json');
+xhr.send();
